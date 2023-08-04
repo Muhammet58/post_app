05f17bd3677ed12925d89222348dbbaf6adbcf70
muhammet <yildirimuhammed058@gamil.com>
2023-08-04 19:28:13 +0300

    post app for testing

--- post_project/settings.py
+++ post_project/settings.py
@@ -118,11 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / 'static', 
-]
-
-STATIC_ROOT = ''
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 # Default primary key field type
